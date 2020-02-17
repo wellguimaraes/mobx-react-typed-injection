f7f51d83f87918ae5349a8dc9569badcd4eb3662
Wellington Guimaraes <wellguimaraes@gmail.com>
2020-02-17 19:45:37 -0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,30 @@
-import * as React from 'react'
 import { observer } from 'mobx-react'
+import * as React from 'react'
 import { DeepPartial } from 'ts-essentials'
 
 export function createStateManager<S>(stores: S) {
-  const StoresContext = React.createContext({} as S)
+  const StoresContext = React.createContext<S>(null as any)
   StoresContext.displayName = 'StoresContext'
 
-  const StoresProvider = ({
+  const StoresProvider: any = <SP extends any = S>({
     stores: _stores = stores as any,
     children,
   }: {
-    stores?: DeepPartial<S>
+    stores?: DeepPartial<SP>
     children: React.ReactNode
   }) => <StoresContext.Provider value={_stores as any}>{children}</StoresContext.Provider>
 
-  const withStores = (stores: DeepPartial<S>) => (toRender: React.ReactNode) => (
-    <StoresProvider stores={stores}>{toRender}</StoresProvider>
-  )
-
-  const inject = <P, I extends Partial<P>>(
-    injector: (stores: S) => I,
-    WrappedComponent: React.FC<P>
-  ): React.FC<Omit<P, keyof I>> =>
-    observer(props => {
-      const stores = React.useContext(StoresContext) as any
-      return <WrappedComponent {...props} {...(injector(stores) as any)} />
-    })
-
   return {
-    stores,
-    inject,
-    withStores,
-    StoresProvider,
+    inject: <P, I extends Partial<P>>(
+      injector: (stores: S) => I,
+      WrappedComponent: React.ComponentType<P>
+    ): React.FC<Pick<P, Exclude<keyof P, keyof I>>> =>
+      observer(props => {
+        const _stores = (React.useContext(StoresContext) as any) || stores || {}
+        return <WrappedComponent {...props} {...(injector(_stores) as any)} />
+      }),
+    withFakeStores: (stores: DeepPartial<S>) => (toRender: React.ReactNode) => (
+      <StoresProvider stores={stores}>{toRender}</StoresProvider>
+    ),
   }
 }
