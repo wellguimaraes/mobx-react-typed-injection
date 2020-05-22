0dca5d762ebc4f4b95c721042d785ab062bf61dc
Wellington Guimaraes <wellguimaraes@gmail.com>
2020-05-22 09:25:38 -0300

    dependencies & types refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,16 @@
-import { IComputedValue } from 'mobx'
 import { observer } from 'mobx-react'
 import * as React from 'react'
 import { DeepPartial } from 'ts-essentials'
 
-type IFunction<T = any> = (...args: any[]) => T
+type InjectedComponent<TProps, TInjection extends Partial<TProps>> = React.FC<
+  Pick<TProps, Exclude<keyof TProps, keyof TInjection>>
+>
 
-export type CleanReturnType<
-  FunctionOrComputed extends IFunction | IComputedValue<any>
-  > = FunctionOrComputed extends IComputedValue<Promise<infer ComputedType>>
-  ? ComputedType
-  : FunctionOrComputed extends IFunction<Promise<infer FunctionReturnType>>
-    ? FunctionReturnType
-    : any
-
-export function createStateManager<S>(stores: S) {
-  const StoresContext = React.createContext<S>(null as any)
+export function createStateManager<TStore>(stores: TStore) {
+  const StoresContext = React.createContext<TStore>(null as any)
   StoresContext.displayName = 'StoresContext'
 
-  const StoresProvider: any = <SP extends any = S>({
+  const StoresProvider: any = <SP extends any = TStore>({
     stores: _stores = stores as any,
     children,
   }: {
@@ -26,17 +19,24 @@ export function createStateManager<S>(stores: S) {
   }) => <StoresContext.Provider value={_stores as any}>{children}</StoresContext.Provider>
 
   return {
-    inject: <P, I extends Partial<P>>(
-      injector: (stores: S) => I,
-      WrappedComponent: React.ComponentType<P>
-    ): React.FC<Pick<P, Exclude<keyof P, keyof I>>> =>
-      observer(props => {
+    inject: <TProps, TInjection extends Partial<TProps>>(
+      injector: (stores: TStore) => TInjection,
+      WrappedComponent: React.ComponentType<TProps>
+    ): InjectedComponent<TProps, TInjection> => {
+      const _InjectedComponent: React.FunctionComponent = props => {
         const _stores = (React.useContext(StoresContext) as any) || stores || {}
         return <WrappedComponent {...props} {...(injector(_stores) as any)} />
-      }),
-    withFakeStores: (stores: DeepPartial<S>) => (toRender: React.ReactNode) => (
-      <StoresProvider stores={stores}>{toRender}</StoresProvider>
+      }
+
+      const wrappedDisplayName = WrappedComponent.displayName || WrappedComponent.name || 'Component'
+      _InjectedComponent.displayName = `MobXTypedInjection(${wrappedDisplayName})`
+
+      return observer(_InjectedComponent)
+    },
+    withFakeStores: (stores: DeepPartial<TStore>) => (children: React.ReactNode) => (
+      <StoresProvider stores={stores}>{children}</StoresProvider>
     ),
   }
 }
+
 
